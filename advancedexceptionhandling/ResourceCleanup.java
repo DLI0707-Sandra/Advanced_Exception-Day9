ca126224564d5c254aef1be07b97d5f1a17681f6
Sandra Davis <Sandra.Davis@godigit.com>
2024-08-02 15:57:03 +0530

    Resource cleanup using try-with-resources

--- advancedexceptionhandling/ResourceCleanup.java
+++ advancedexceptionhandling/ResourceCleanup.java
@@ -11,6 +11,17 @@ public class ResourceCleanup
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter the file to be read:");
         String filename=scanner.next();
+        /*Using Try with resources*/
+        try(FileReader fileReader=new FileReader(filename);BufferedReader bufferedReader=new BufferedReader(fileReader); )
+        {
+            System.out.println("Content of file "+filename);
+            String data=bufferedReader.readLine();
+            System.out.println(data);
+        }catch (IOException e)
+        {
+            System.out.println("Unable to open file "+filename);
+        }
+        /* Clean up using finally
         BufferedReader bufferedReader=null;
         try {
             bufferedReader=new BufferedReader(new FileReader(filename));
@@ -23,7 +34,7 @@ public class ResourceCleanup
             if(bufferedReader!=null)
                 bufferedReader.close();
         }
-
+        */
 
     }
 }
